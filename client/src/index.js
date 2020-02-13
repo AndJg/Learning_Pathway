271a731abea750adc69a23ee92d8da0b23313c55
Andrzej <eviljajo@gmail.com>
2020-02-13 13:56:44 +0100

    redux configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Root from './root';
-import rootReducer from './reducers/rootReducer';
-
-const store = createStore(rootReducer);
+import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
